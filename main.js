50a33200df2bff16324ba258e12f9b05358b9c2d
Sebastian Sitaru <sebastian@mail.sitaru.eu>
2012-03-31 06:08:30 +0200

    first test

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var portlist = [
-{'port': 80, 'service': 'HTTP'},
 {'port': 22, 'service': 'SSH'},
 {'port': 21, 'service': 'FTP'},
 {'port': 25, 'service': 'SMTP'},
@@ -7,39 +6,46 @@ var portlist = [
 
 function getPortStatus(o) 
 {
-	o.foreach(function(k, v){
+	o.forEach(function(v, k){
 		$.delay(2000);
 		var postData = {'port': v['port']};
 		$.post('/heartbeat/check.php', postData, function(data){
-				
+				if(data['return'])
+				{
+					$('#td3_'+v['service']).text('ok').addClass('online');
+				} else {
+					$('#td3_'+v['service']).text('failed').addClass('offline');
+				}
 		});
 	});
 }
 
 function initPortStatus(o)
 {
-	o.foreach(function(k, v){
+	o.forEach(function(v, k){
 		var tr_id = 'tr_'+v['service'];
 		var td1_id = 'td1_'+v['service'];
 		var td2_id = 'td2_'+v['service'];
 		var td3_id = 'td3_'+v['service'];
 		if($('#'+tr_id).length <= 0) 
 		{
-			$('#maintable tbody').add('tr').attr('id', tr_id);
+			$('#maintable tr:last').after('<tr id="'+tr_id+'"></tr>');
 		}
 		if($('#'+td1_id).length <= 0) 
 		{
-			$('#'+tr_id).add('td').attr('id', td1_id).text('*:'+v['port']);
+			$('#'+tr_id).append('<td id="'+td1_id+'">*:'+v['port']+'</td>');
 		}
 		if($('#'+td2_id).length <= 0) 
 		{
-			$('#'+tr_id).add('td').attr('id', td2_id).text(v['service']);
+			$('#'+tr_id).append('<td id="'+td2_id+'">'+v['service']+'</td>');
 		}
 		if($('#'+td3_id).length <= 0) 
 		{
-			$('#'+tr_id).add('td').attr('id', td3_id).addClass('checking').text('checking...');
+			$('#'+tr_id).append('<td id="'+td1_id+'" class="checking">checking...</td>');
 		}
 	});
 }
+$(document).ready(function(){
+	initPortStatus(portlist);
+});
 
-initPortStatus(portlist);
